f2728bf1d0daa0c9f43b131e19da34aa08c9f73c
ShreyaAgrawal-jp <63266009+ShreyaAgrawal-jp@users.noreply.github.com>
2021-05-10 12:32:35 +0530

    Added buttons and various navigators
    stack navigator, drawer navigator and bottom tab navigator

--- App.js
+++ App.js
@@ -1,22 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+function HomeScreen({ navigation,route }) {React.useEffect(() => {
+  if (route.params?.post) {
+    // Post updated, do something with `route.params.post`
+    // For example, send the post to the server
+  }
+}, [route.params?.post]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hiii!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        })}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      /> 
+      <Button title="Go to Home" 
+      onPress={() => navigation.navigate('Home')} 
+      />
+      <Button title="Go back" 
+      onPress={() => navigation.goBack()} 
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" 
+        component={HomeScreen} 
+        options={{ title: 'Overview' }}
+        />
+        <Drawer.Screen name="Details" 
+        component={DetailsScreen} 
+        initialParams={{ itemId: 42 }} 
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
